885ce9581034c67cc47ebf56cf657f7bea2b6ab9
edgd1er <edgd1er@hotmail.com>
2016-02-17 18:25:26 +0100

    removed unused function

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -28,42 +28,4 @@ class DefaultController extends Controller
         ]);
     }
 
-    /**
-     * @Route("/unauthorized", name="unauthorized")
-     *
-     */
-    public function unauthorizedAction()
-    {
-
-        $token = $this->get('security.context')->getToken();
-
-        //Si user loggé
-        if (isset($token))
-        {
-            $user = $this->get('security.context')->getToken()->getUser();
-            $roles = $user->getRoles();
-
-            switch ($roles[0])
-            {
-                //TODO: Page d'inscription
-                case User::ROLE_INACTIVE: $mess = 'Redirection to SignUp form (INACTIVE)';
-                    break;
-                //TODO: Page de renseignement CB
-                case User::ROLE_ACTIVE_CC_PROBLEM: $mess = 'Redirection to CB form (ACTIVE_CC_PROBLEM)';
-                    break;
-                //TODO: Page de réabonnement
-                case User::ROLE_ACTIVE_NO_ACCESS: $mess = 'Redirection subscription form (ACTIVE_NO_ACCESS)';
-                    break;
-                default: $mess = 'Default redirection';
-                    break;
-            }
-        }
-        //Sinon, redirection sur logout -> renvoie sur login
-        else
-        {
-            return $this->redirect($this->generateUrl('form_login'));
-        }
-
-        return $this->render('error403.html.twig', array ('mess' => $mess));
-}
 }
